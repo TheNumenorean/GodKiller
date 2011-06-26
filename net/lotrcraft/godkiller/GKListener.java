8576dba504274d55b7bdbb27fc9492373bb72be4
Francesco Macagno <fjmacagno1@yahoo.com>
2011-06-26 12:48:09 -0700

    First Version

---
+++ net/lotrcraft/godkiller/GKListener.java
@@ -0,0 +1,24 @@
+package net.lotrcraft.godkiller;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityListener;
+
+public class GKListener extends EntityListener{
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event instanceof EntityDamageByEntityEvent){
+			EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
+			if (e.getDamager() instanceof Player){
+				Player dmgr = (Player) e.getDamager();
+				if (!GKMain.permissionHandler.has(dmgr, "GK.kill")) {
+				      return;
+				}
+				e.getEntity().getLastDamageCause().setCancelled(false);
+				
+				
+			}
+		}
+	}
+
+}
---
+++ net/lotrcraft/godkiller/GKMain.java
@@ -0,0 +1,50 @@
+package net.lotrcraft.godkiller;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.Event;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+public class GKMain extends JavaPlugin {
+	
+	public static PermissionHandler permissionHandler;
+	Logger log = Logger.getLogger("minecraft");
+	Configuration config;
+
+	@Override
+	public void onDisable() {
+		log.info("GodKiller disabled.");
+
+	}
+
+	@Override
+	public void onEnable() {
+		log.info("GodKiller enabled.");
+		setupPermissions();
+		config = this.getConfiguration();
+		this.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DAMAGE, new GKListener(), Event.Priority.Highest, this);
+
+	}
+	
+	private void setupPermissions() {
+	    if (permissionHandler != null) {
+	        return;
+	    }
+	    
+	    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+	    
+	    if (permissionsPlugin == null) {
+	        log.info("Permission system not detected, defaulting to OP");
+	        return;
+	    }
+	    
+	    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+	    log.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+	}
+
+}
